32d179d7c6f8e520a28a9eb7004f0bc00f710476
BrayanSGL <bsgaleanol@udistrital.edu.co>
2025-01-23 14:09:47 -0500

    Agregar componente Card y su integración en la página de inicio

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
+
 import Layout from "../../components/Layout";
+import Card from "../../components/Card";
 
 function Home() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <>
       <Layout>
         <h1 className="text-2xl font-bold">Home</h1>
+        <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+          {products &&
+            products.map((product) => (
+              <Card key={product.id} data={product} />
+            ))}
+        </div>
       </Layout>
     </>
   );
